252b3eb7b718326dbdf7ee2c954a71bf4d515168
totorroto <oyhanart.tomas@gmail.com>
2023-05-21 23:05:47 -0300

    FIXED 2

--- ej2.cpp
+++ ej2.cpp
@@ -1,12 +1,11 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-vector<vector<long long>> adj, adj_rev;
+vector<vector<long long>> adj;
 
 vector<bool> used;
-vector<long long> order, component;
-vector<vector<long long>> dag;
-vector<bool> visited;
+vector<long long> order;
+
 vector<long long> res;
 
 void dfs1(long long v) {
@@ -21,39 +20,19 @@ void dfs1(long long v) {
 
 void dfs2(long long v) {
     used[v] = true;
-    component.push_back(v);
 
-    for (auto u : adj_rev[v]){
+    for (auto u : adj[v]){
         if (!used[u])
             dfs2(u);
     }
 }
 
-void dfs3(long long v) {
-    visited[v] = true;
-    for (long long u : dag[v]) {
-        if (!visited[u])
-            dfs3(u);
-    }
-    
-}
-
-void topological_sort() {
-    visited.assign(dag.size(), false);
-    for (long long i = 0; i < dag.size(); i++) {
-        if (!visited[i]){
-            dfs3(i);
-            res.push_back(i);
-        }
-    }
-}
 
 int main() {
     long long n,m;
     cin>>n>>m;
     
     adj.resize(n);
-    adj_rev.resize(n);
    
     while(m--) {
         long long a, b;
@@ -61,7 +40,6 @@ int main() {
         a--;
         b--;
         adj[a].push_back(b);
-        adj_rev[b].push_back(a);
     }
     
     used.assign(n, false);
@@ -69,44 +47,23 @@ int main() {
         if (!used[i])
             dfs1(i);
     }
+
     used.assign(n, false);
     reverse(order.begin(), order.end());
-    vector<long long> temp(n);
-
     for (auto v : order){
         if (!used[v]) {
             dfs2 (v);
-            for(long long u : component){
-                temp[u] = v;
-            }
-            component.clear();
+            res.push_back(v);
         }
     }
-    
-    for(long long i =0;i<n;i++){
-        if(temp[i]==i){
-            component.push_back(i);
-        }
-    }
-    dag.resize(component.size());
-    vector<long long> posOcupada(n);
-    for(long long i =0;i<component.size();i++){
-        posOcupada[component[i]] = i;
-    }
-
-    for(long long i =0;i<n;i++){
-        for(long long j =0;j<adj[i].size();j++){
-            if(temp[i]!=temp[adj[i][j]]){
-                dag[posOcupada[temp[i]]].push_back(posOcupada[temp[adj[i][j]]]);
-            }
-        }
-    }
-
-    topological_sort();
     cout<<res.size()<<endl;
-    for(long long i =0;i<res.size();i++){
-        cout<<component[res[i]]+1<<" ";
+    reverse(res.begin(), res.end());
+    for(auto i:res) {
+        cout<<i+1<<" ";
     }
+
+    
+
     
     return 0;
     
